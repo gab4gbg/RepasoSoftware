9e11ef95773af2c69e3343b6ff44154a96771b49
gab <gabrielgenarobg@gmail.com>
2026-02-17 00:08:10 -0500

    implementacion bibliotecario

--- src/main/java/org/yourcompany/yourproject/Repaso.java
+++ src/main/java/org/yourcompany/yourproject/Repaso.java
@@ -5,6 +5,7 @@ public class Repaso
 {
 	public static void main(String args[])
 	{
-        IO.println("hola mundo");
+		Bibliotecario bibliotecario = new Bibliotecario("1", "Pepe", "pepe@gmail.com", "Mañana");
+		System.out.println(bibliotecario.getEmail());
 	}
 }
