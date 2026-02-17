fb4b22bafe18bafbca364cd8d70e39b6384a43b7
gab <gabrielgenarobg@gmail.com>
2026-02-17 01:37:09 -0500

    desarrollo clase Ejemplar

--- src/main/java/org/yourcompany/yourproject/Ejemplar.java
+++ src/main/java/org/yourcompany/yourproject/Ejemplar.java
@@ -2,4 +2,22 @@ package org.yourcompany.yourproject;
 
 public class Ejemplar
 {
+    private String codigo;
+    private Libro libro;
+
+    public  Ejemplar(String codigo, Libro libro)
+    {
+        this.codigo = codigo;
+        this.libro = libro;
+    }
+
+    public String getCodigo()
+    {
+        return codigo;
+    }
+
+    public Libro getLibro()
+    {
+        return libro;
+    }
 }
